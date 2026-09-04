00b61cc88e40f96b4a70d3987848373f025cd6bb
haseebjamil00 <haseebjamilqureshi72@gmail.com>
2026-09-04 16:19:05 +0500

    Add files via upload

---
+++ assets/blog-config.js
@@ -0,0 +1,7 @@
+// Fill these in once the GitHub repo is created, then this powers both blog.html and blog-post.html
+window.BLOG_CONFIG = {
+  owner: "haseebjamil00",
+  repo: "ajaconsultancy",
+  branch: "main",
+  contentPath: "content/blog"
+};
